26dd6f1b78172e8c62b6158a18edd9e87d51acd8
kratika.chowdhary <kratika.chowdhary@servicenow.com>
2022-06-19 14:05:42 +0530

    Refactor code, add folder structure

---
+++ lib/src/widgets/form_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryColor = Colors.pink;
+const TextStyle textStylePrimaryColor = TextStyle(color: primaryColor);
+
+class CustomFormField extends StatelessWidget {
+  final String stepNumber;
+  final String fieldLabel;
+  final String buttonLabel;
+  final Function? fieldValidation;
+  final String hint;
+  final VoidCallback? buttonClickHandler;
+  const CustomFormField(
+    String s, {
+    this.stepNumber = '',
+    required this.fieldLabel,
+    this.buttonLabel = '',
+    required this.hint,
+    this.buttonClickHandler,
+    this.fieldValidation,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Divider(height: 50),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Container(
+              margin: const EdgeInsets.only(right: 50),
+              child: Text(stepNumber,
+                  style: const TextStyle(fontSize: 50.0, color: Colors.grey)),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration:
+                      InputDecoration(hintText: hint, labelText: fieldLabel),
+                  validator: (value) => fieldValidation!(value),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        minimumSize: const Size.fromHeight(50)),
+                    onPressed: null,
+                    child: Text(buttonLabel,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 15)),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ]);
+  }
+}
