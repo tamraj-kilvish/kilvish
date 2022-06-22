dcb445a117146f90496a2e21998467ef5516c321
pocha <grandmastrgogo@gmail.com>
2022-06-22 23:49:36 +0100

    temp commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,20 @@ const TextStyle textStylePrimaryColor = TextStyle(color: primaryColor);
 
 const MaterialColor inactiveColor = Colors.grey;
 const TextStyle textStyleInactive = TextStyle(color: inactiveColor);
+
+Widget appBarMenu(Function()? onPressedAction) {
+  return IconButton(
+    icon: const Icon(Icons.menu),
+    onPressed: onPressedAction,
+  );
+}
+
+Widget appBarSearch(Function()? onPressedAction) {
+  return IconButton(
+    icon: Icon(
+      Icons.search,
+      color: Colors.white,
+    ),
+    onPressed: null,
+  );
+}
