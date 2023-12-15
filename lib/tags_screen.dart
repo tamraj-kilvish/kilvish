7972448a0c2debddf10f92456f4ec029b6acc115
Ashish Sharma <ashish@catapultlabs.xyz>
2023-12-15 14:38:32 +0530

    On tags screen, now there are two options - add/remove & view/edit

--- lib/tags_screen.dart
+++ lib/tags_screen.dart
@@ -120,7 +120,9 @@ class _TagsPageState extends State<TagsPage> {
             child: renderPrimaryColorLabel(text: 'Attached Tags'),
           ),
           _renderTagGroup(
-              tags: _attachedTagsFiltered, status: TagStatus.selected),
+              tags: _attachedTagsFiltered,
+              status: TagStatus.selected,
+              context: context),
           Container(
             margin: const EdgeInsets.only(top: 5.0),
             child: const Divider(height: 10),
@@ -132,17 +134,20 @@ class _TagsPageState extends State<TagsPage> {
               renderHelperText(text: 'only 10 tags are shown')
             ]),
           ),
-          _renderTagGroup(tags: _unselectedTagsFiltered),
+          _renderTagGroup(tags: _unselectedTagsFiltered, context: context),
         ]),
       ),
-      bottomNavigationBar: BottomAppBar(child: renderMainBottomButton('Done',(){
-        Navigator.pop(context,_attachedTags);
+      bottomNavigationBar: BottomAppBar(
+          child: renderMainBottomButton('Done', () {
+        Navigator.pop(context, _attachedTags);
       })),
     );
   }
 
   Widget _renderTagGroup(
-      {required Set<Tag> tags, TagStatus status = TagStatus.unselected}) {
+      {required Set<Tag> tags,
+      TagStatus status = TagStatus.unselected,
+      required BuildContext context}) {
     if (tags.isEmpty) {
       return const Text('No tags found ..',
           style: TextStyle(color: inactiveColor));
@@ -158,12 +163,52 @@ class _TagsPageState extends State<TagsPage> {
             text: tag.name,
             status: status,
             isUpdated: _modifiedTags.containsKey(tag) ? true : false,
-            onPressed: () => executeOnTagButtonPress(tag: tag, status: status));
+            onPressed: () =>
+                onTagPressed(tag: tag, status: status, context: context));
       }).toList(),
     );
   }
 
-  void executeOnTagButtonPress({required Tag tag, required TagStatus status}) {
+  Future<void> onTagPressed(
+      {required Tag tag,
+      TagStatus status = TagStatus.unselected,
+      required BuildContext context}) async {
+    String? userAction = await showDialog<String>(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, "Add/Remove");
+                },
+                child: status == TagStatus.selected
+                    ? Text('Remove ${tag.name}')
+                    : Text('Add ${tag.name}'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, "View/Edit");
+                },
+                child: Text('View/Edit ${tag.name}'),
+              ),
+            ],
+          );
+        });
+
+    switch (userAction) {
+      case "Add/Remove":
+        selectDeselectTag(tag: tag, status: status);
+        break;
+      case "View/Edit":
+        // TODO - send to add/edit tag screen .. as of now, just sending to tags page again
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const TagsPage()));
+        break;
+    }
+  }
+
+  void selectDeselectTag({required Tag tag, required TagStatus status}) {
     setState(() {
       _modifiedTags.remove(tag);
 
